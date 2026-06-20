5230a117da35b0fdf80ad6721f057a9d23cc5709
chenmo000 <59405275+chenmo000@users.noreply.github.com>
2026-06-20 20:27:50 +0800

    Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -90,6 +90,7 @@ UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
 #UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 #UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
+UPDATE_PACKAGE "dockerman" "lisaac/luci-app-dockerman" "master"
 
 #更新软件包版本
 UPDATE_VERSION() {
